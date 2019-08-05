4ba386f6a35a5625c8f385deaef8539be5096b7f
Paul Buss <cyberserfer@gmail.com>
2019-08-05 10:51:42 -0500

    added schedule and speakers empty pages

---
+++ pages/schedule.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.css";
+import Layout from "../components/Layout";
+
+const Schedule = () => 
+    <Layout>
+        <div> schedule </div>
+    </Layout>;
+
+export default Schedule;
---
+++ pages/speakers.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.css";
+import Layout from "../components/Layout";
+
+const Speakers = () => 
+    <Layout>
+        <div> speakers </div>
+    </Layout>;
+
+export default Speakers;
